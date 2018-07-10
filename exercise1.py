e94d5ccc76c696d234cce0eb53417ab339f03bcb
Liza Avramenko <liza.avramenko@gmail.com>
2018-07-10 23:40:57 +0300

    Initial commit

---
+++ exercise1.py
@@ -0,0 +1,16 @@
+def hundredyearsold():
+    name = input("Tell me your name: ")
+    age = int (input("Tell me your age: "))
+    year = int (input ("What year is it today? "))
+    copy = int (input ("How many times do you want me to repeat this? "))
+
+    print (((name + ", you will turn 100 years old in " + str((100-age)+year))+"\n") * copy)
+
+
+
+
+
+
+
+
+
