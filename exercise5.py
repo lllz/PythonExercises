43bdd815c85ee24111746ad8364054ce6de8dcdf
Liza Avramenko <liza.avramenko@gmail.com>
2018-07-12 18:07:37 +0300

    Fifth exercise

---
+++ exercise5.py
@@ -0,0 +1,16 @@
+import random
+
+def commonEl():
+    a = random.sample(range(0,100), 25)
+    b = random.sample(range(0, 100), 45)
+    print(a)
+    print(b)
+    print ("***********" *3)
+    result = []
+
+    for i in a:
+        for j in b:
+            if i == j:
+                if i not in result:
+                    result.append(i)
+    return (result)
